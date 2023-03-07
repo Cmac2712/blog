3dbf7bdd6d62e00b21a9578f48cb2039246bf77d
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2023-03-07 21:47:10 +0000

    Making some components

---
+++ src/components/FormattedDate.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let date;
+</script>
+
+<time class="text-xs text-gray-400" datetime={date.toISOString()}>
+	{
+		date.toLocaleDateString('en-us', {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric',
+		})
+	}
+</time>
