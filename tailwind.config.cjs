67266e8b14f919bc71e9b094d2f9515444fcd0dd
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2023-03-23 21:53:01 +0000

    Added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [require('@tailwindcss/typography'), require('daisyui')],
-	daisyui: {
-		themes: true,
-	}
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["lato", ...defaultTheme.fontFamily.sans],
+        serif: ["arvo", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  daisyui: {
+    themes: true,
+  },
+};
