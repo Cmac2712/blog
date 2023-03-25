9e2ec6dc38484c4c65373706d89d47e21d699440
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2023-03-25 21:12:51 +0000

    feat: added more styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Craig MacIntyre";
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Craig MacIntyre's Blog";
+export const SITE_DESCRIPTION = 'Welcome to my blog where I write things.';
