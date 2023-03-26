caae1dcf67431ae9a2a7bc77fb2a0c2fcbc80f81
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2023-03-26 21:36:21 +0100

    feat: added Bio and Social components

---
+++ src/components/Bio.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { PROFILE_DESCRIPTION, PROFILE_PIC } from "../consts";
+	export let content = PROFILE_DESCRIPTION;
+    export let pic = PROFILE_PIC;
+    export let className = '';
+</script>
+
+<div class={`flex items-center ${className}`}>
+    <img class={`rounded-full w-16 mr-2`} src={pic} alt={"Profile picture of Craig MacIntyre"}>
+    <p class="text-sm text-neutral-content">{@html content}</p>
+</div>
--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = "Craig MacIntyre's Blog";
 export const SITE_DESCRIPTION = 'Welcome to my blog where I write things.';
+export const PROFILE_DESCRIPTION = `Hello! I\'m <a href="https://twitter.com/cmac2712" class="underline" target="_blank">Craig.</a><br /> I occasionally write about web development&mdash;React, Typescript, Best Practices or whatever else I feel like writing about. `;
+export const PROFILE_PIC = 'https://avatars.githubusercontent.com/u/4986039';
