822b03c020edcd7363bd86b90f4128cf94bdbc78
Craig MacIntyre <craig.macintyre@thepetlabco.com>
2023-11-04 22:19:05 +0000

    feat: refactor

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -1,11 +1,17 @@
 <script>
     import { PROFILE_DESCRIPTION, PROFILE_PIC } from "../consts";
-	export let content = PROFILE_DESCRIPTION;
     export let pic = PROFILE_PIC;
     export let className = '';
 </script>
 
 <div class={`flex items-center ${className}`}>
     <img class={`rounded-full w-16 mr-2`} src={pic} alt={"Profile picture of Craig MacIntyre"} width={'64'} height={'64'}>
-    <p class="text-sm text-neutral-content">{@html content}</p>
+    <div>
+        <p class="text-sm text-neutral-content m-0">
+            {@html `Hello! I\'m Craig and I build things on the web.`}
+        </p>
+        <p class="text-sm text-neutral-content m-0">
+            I also occasionally <a href="/blog">write</a> the odd thing about web development.
+        </p>
+    </div>
 </div>
--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Craig MacIntyre's Blog";
-export const SITE_DESCRIPTION = 'Welcome to my blog where I write things.';
-export const PROFILE_DESCRIPTION = `Hello! I\'m <a href="https://twitter.com/cmac2712" class="underline" target="_blank">Craig.</a><br /> I occasionally write about web development&mdash;React, Typescript, Best Practices or whatever else I feel like writing about. `;
-export const PROFILE_PIC = 'https://avatars.githubusercontent.com/u/4986039';
+export const SITE_TITLE = "Craig MacIntyre";
+export const SITE_DESCRIPTION = "I'm Craig. I'm a web developer.";
+export const PROFILE_PIC = "https://avatars.githubusercontent.com/u/4986039";
